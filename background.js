9e5cf1b5082e3f10f13cad01daaa01c030a790d1
Taylor Martin <taylor.martin357@gmail.com>
2014-03-06 02:28:20 -0500

    Internal email image reference

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
+var imgEmail = chrome.extension.getURL("/images/email.png");
 var dl = document.querySelector('.d2l-minibar-activity');
 var gotoemail = dl.childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].getAttribute('href');
-dl.childNodes[7].innerHTML = '<a href="'+gotoemail+'"><img src="https://raw.github.com/tmartin357/D2L-Corrections/master/email.png" alt="D2L Email"></a>';
+dl.childNodes[7].innerHTML = '<a href="'+gotoemail+'"><img src="'+imgEmail+'" alt="D2L Email"></a>';
 var attr = dl.childNodes[7].getAttribute('class');
 dl.childNodes[7].setAttribute('class', 'D2L-Correct-email-link '+attr);
